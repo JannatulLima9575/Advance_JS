ae9ffa1e5b6efbd372e0e872fa0ac755525b14e1
Jannatul <limajannat1675@gmail.com>
2025-11-27 19:56:25 +0600

    added  lab test problems

--- game.js
+++ game.js
@@ -1,4 +1,11 @@
 let apple = 10;
 let banana = parseInt('20');
+let cherry = Number('30');
+let value= parseFloat('40.5');
+let date = new Date('2023-01-01');
 
 console.log(apple + banana);
+console.log(value);
+console.log(cherry);
+
+console.log(date.getFullYear());
