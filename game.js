75aa74fec2ae263b462dd15a6a2036c815a52548
Jannatul <limajannat1675@gmail.com>
2025-11-28 18:48:54 +0600

    added  lab test problems

--- game.js
+++ game.js
@@ -3,9 +3,12 @@ let banana = parseInt('20');
 let cherry = Number('30');
 let value= parseFloat('40.5');
 let date = new Date('2023-01-01');
+let invalidNumber = Number('not a number');
+
 
 console.log(apple + banana);
 console.log(value);
 console.log(cherry);
 
 console.log(date.getFullYear());
+console.log(isNaN(invalidNumber));
