6cc38af133a04ab730450b1718e61a2140be4e03
Jannatul <limajannat1675@gmail.com>
2025-12-22 20:39:30 +0600

    finish basic document object model

--- DOM_Concept/main.js
+++ DOM_Concept/main.js
@@ -7,4 +7,19 @@ const liCollection = document.getElementsByTagName("li");
 for(const li of liCollection){
     console.log(li.innerText);
 }
-    
+
+// getElementById and getElementsByClassName can also be used similarly.
+// You can also manipulate the DOM elements as needed.
+const popularHeading = document.getElementById("title2");
+popularHeading.style.color = "Green";
+
+const title2Text = document.getElementById("title2").innerText;
+console.log(title2Text);
+document.getElementById("title2").innerText = "Top DOM Methods Updated";
+
+const classElements = document.getElementsByClassName("paragraph1");
+classElements.style.color = "Purple";
+for(const elem of classElements){
+    elem.style.fontWeight = "bold";
+    console.log(elem.innerText);
+}
